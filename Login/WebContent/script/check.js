09ea99c04f2f1896591593e0e4428bc96dd6cd14
slskfl <62872578+slskfl@users.noreply.github.com>
2021-04-13 10:54:40 +0900

    Update check.js

--- Login/WebContent/script/check.js
+++ Login/WebContent/script/check.js
@@ -3,7 +3,7 @@ function validation(){
 		alert("아이디를 입력하세요.");
 		document.frm.userID.focus();
 		return false;
-	} else if(document.frm.userID.value.lenght>10){
+	} else if(document.frm.userID.value.length>10){
 		alert("아이디를 10글자 이하로 입력하세요.");
 		document.frm.userID.focus();
 		return false;
@@ -12,7 +12,7 @@ function validation(){
 		alert("비밀번호를  입력하세요.");
 		document.frm.passwd.focus();
 		return false;
-	}else if(document.frm.passwd.value.lenght>10){
+	}else if(document.frm.passwd.value.length>10){
 		alert("비밀번호를 10글자 이하로 입력하세요.");
 		document.frm.passwd.focus();
 		return false;
